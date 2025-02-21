d3c7dfc14b2f3872bf4abd1c5b1a56edb176e397
Mohamed Saied <mohamed230104326@sut.edu.eg>
2025-02-21 04:16:14 +0200

    Complete Assignment 1

---
+++ routes/web.php
@@ -0,0 +1,20 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+Route::get('/multable/{number?}', function ($number = 8) {
+    $j = $number;
+    return view('multable', compact("j") );
+});
+Route::get('/even', function () {
+    return view('even');
+});
+Route::get('/prime', function () {
+    return view('prime');
+});
+Route::get('/test', function () {
+    return view('test');
+});
